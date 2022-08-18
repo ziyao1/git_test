234f0c3989891c455be71add3d2f6c20d0424e86
zhan xingguang <3306892812@qq.com>
2022-08-18 17:38:18 +0800

    push commit

--- src/main/java/com/example/git_test/gitTest.java
+++ src/main/java/com/example/git_test/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("git test4");
         System.out.println("master commit");
         System.out.println("hot-fix commit");
+        System.out.println("push commit");
     }
 }
